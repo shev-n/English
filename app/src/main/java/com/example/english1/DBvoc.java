2fa9aa81e432b99a0d03dddf5d87fd71467451bb
shev-n <shevyakinnik@mail.ru>
2020-05-24 12:33:03 +0300

    English

--- app/src/main/java/com/example/english1/DBvoc.java
+++ app/src/main/java/com/example/english1/DBvoc.java
@@ -1,6 +1,5 @@
 package com.example.english1;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,33 +7,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBvoc extends SQLiteOpenHelper {
-
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "test2";
-    public static final String TABLE_NAME = "voc";
+    public static final String DATABASE_NAME = "english";
+    public static final String TABLE_CONSTANTS = "constants";
 
     public static final String KEY_ID = "_id";
-    public static final String KEY_WORD = "WORD";
-    public static final String KEY_TRANSLATE = "TRANSLATE";
+    public static final String KEY_RUSSIAN = "russianWords";
+    public static final String KEY_ENGLISH = "englishWords";
 
     public DBvoc(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public DBvoc(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + "voc" + "(" + KEY_ID
-                + " integer primary key," + KEY_WORD + " text," + KEY_TRANSLATE + " text" + ")");
+        db.execSQL("create table " + TABLE_CONSTANTS + "(" + KEY_ID
+                + " integer primary key," + KEY_RUSSIAN + " text," + KEY_ENGLISH + " text" + ")");
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_NAME);
+
+        db.execSQL("drop table if exists " + TABLE_CONSTANTS);
 
         onCreate(db);
-
     }
 }
--- app/src/main/java/com/example/english1/voc.java
+++ app/src/main/java/com/example/english1/voc.java
@@ -3,17 +3,46 @@ package com.example.english1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class voc extends AppCompatActivity {
+public class voc extends AppCompatActivity implements View.OnClickListener {
+    DBvoc dBhelper;
+    public String russianWord = "Компьютер";
+    public String englishWord = "Computer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voc);
+        dBhelper = new DBvoc(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        //добавление данных
+        SQLiteDatabase database = dBhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBvoc.KEY_RUSSIAN, russianWord);
+        contentValues.put(DBvoc.KEY_ENGLISH, englishWord);
+        database.insert(DBvoc.TABLE_CONSTANTS, null, contentValues);
+        Cursor cursor = database.query(DBvoc.TABLE_CONSTANTS, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBvoc.KEY_ID);
+            int russianIndex = cursor.getColumnIndex(DBvoc.KEY_RUSSIAN);
+            int englishIndex = cursor.getColumnIndex(DBvoc.KEY_ENGLISH);
+            do{
+                //куда выводить данные с таблицы, после добавим
+            }
+            while (cursor.moveToFirst());
+        }else {
+            cursor.close();//закрытие курсора после вывода
+        }
+        dBhelper.close();//закрытие базы после вывода
     }
 }
